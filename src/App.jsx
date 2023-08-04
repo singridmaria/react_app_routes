7f69b1eb86f8c129d9e40d846979b43048733eef
singridmaria <singrid.maria@gmail.com>
2023-08-04 00:49:35 -0300

    Testando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { MenuBar } from './components/MenuBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <MenuBar/>
     </>
   )
 }
